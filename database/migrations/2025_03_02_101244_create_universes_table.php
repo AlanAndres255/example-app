99c6f793a8b27d169ec46f903bcab2e0259108e7
AlanAndres255 <perezlozanoalanandres@gmail.com>
2025-03-02 05:36:58 -0600

    seeders

---
+++ database/migrations/2025_03_02_101244_create_universes_table.php
@@ -0,0 +1,23 @@
+<?php
+// filepath: database/migrations/xxxx_xx_xx_xxxxxx_create_universes_table.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('universes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique(); // universe name
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('universes');
+    }
+};
---
+++ database/seeders/UniverseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Universe;
+
+class UniverseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Universe::create(['name' => 'Marvel']);
+        Universe::create(['name' => 'DC']);
+    }
+}
