be52c50c1b8af10a31350341ad58f184f7b6b3a8
AlanAndres255 <perezlozanoalanandres@gmail.com>
2025-03-07 03:42:51 -0600

    Actividad 8

---
+++ resources/views/gender.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gender Test</title>
+</head>
+<body>
+    <h1> Gender</h1>
+        <table>
+            <thead>
+                <th>Id</th>
+                <th>Name</th>
+            </thead>
+            <tbody> 
+               @foreach($gender as $item)
+                <tr>
+                    <td>{{$item -> id}}</td>
+                    <td>{{$item -> name}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <!--<pre> {{print_r($gender)}}</pre>-->
+</body>
+</html>
